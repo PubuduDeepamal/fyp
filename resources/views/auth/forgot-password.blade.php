6de28871e5164de285ed1ebc03e212edd78f50df
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 03:44:23 +0530

    Update forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,3 +1,24 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>JackPets.lk</title>
+
+    <link rel="icon" type="image/png" href="assets/css/JACKPETS.png">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+
+    @include('user.heder1')
+
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
@@ -32,3 +53,5 @@
         </form>
     </x-authentication-card>
 </x-guest-layout>
+
+@include('user.footer')
