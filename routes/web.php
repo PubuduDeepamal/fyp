9e0acc9c796dae7ee4faa6ab3d7788c5697b1957
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 03:44:46 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AdminController;
 
 Route::middleware([
     'auth:sanctum',
@@ -28,3 +19,31 @@ Route::middleware([
 route::get('/redirect',[HomeController::class,'redirect']);
 
 route::get('/',[HomeController::class,'index']);
+
+route::get('/product',[AdminController::class,'product']);
+
+route::post('/uploadproduct',[AdminController::class,'uploadproduct']);
+
+route::get('/showproduct',[AdminController::class,'showproduct']);
+
+route::get('/deleteproduct/{id}',[AdminController::class,'deleteproduct']);
+
+route::get('/updateview/{id}',[AdminController::class,'updateview']);
+
+route::post('/updateproduct/{id}',[AdminController::class,'updateproduct']);
+
+route::get('/search',[HomeController::class,'search']);
+
+route::post('/addcart/{id}',[HomeController::class,'addcart']);
+
+route::get('/showcart',[HomeController::class,'showcart']);
+
+route::get('/delete/{id}',[HomeController::class,'deletecart']);
+
+route::post('/order',[HomeController::class,'confirmorder']);
+
+route::get('/showorder',[adminController::class,'showorder']);
+
+route::get('/updatestatus/{id}',[adminController::class,'updatestatus']);
+
+route::get('/Homeproduct',[HomeController::class,'Homeproduct']);
