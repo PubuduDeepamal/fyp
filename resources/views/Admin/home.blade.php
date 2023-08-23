f8af69a9860335fb6e963c6ee0da829bd196dba5
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-24 03:44:04 +0530

    Update home.blade.php

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -1,3 +1,12 @@
-<x-app-layout>
-
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      @include('admin.css')
+  </head>
+  <body>
+      @include('admin.sidebar')
+      @include('admin.navbar')
+      @include('admin.body')
+      @include('admin.script')    
+  </body>
+</html>
